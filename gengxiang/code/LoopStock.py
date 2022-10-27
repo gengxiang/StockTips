479316ec9b561426a16cd8e3343899c9e2ff20b9
gengxiang <gengxiang1024@163.com>
2022-10-28 07:16:43 +0800

    full dump batch

--- gengxiang/code/LoopStock.py
+++ gengxiang/code/LoopStock.py
@@ -530,9 +530,9 @@ l_num = 0
 page_size = 20
 while l_num < len(all_stock_code):
     print(l_num, '~',  l_num + page_size, "->", all_stock_code[l_num: l_num + page_size])
-    l_num = l_num + page_size
     select_list.extend(full_dump_list(all_stock_code[l_num: l_num + page_size]))
     time.sleep(0.1 + random.random())
+    l_num = l_num + page_size
 print(select_list)
 
 
