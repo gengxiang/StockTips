1f01fa109ddea1f2276a459a3ff00a19be31e6fb
gengxiang <gengxiang1024@163.com>
2023-04-25 22:37:22 +0800

    daliy

--- gengxiang/code/LoopBK.py
+++ gengxiang/code/LoopBK.py
@@ -725,24 +725,10 @@ def get_bk_file():
 # 今天日期
 todayStr = time.strftime('%Y-%m-%d', time.localtime(time.time()))
 
-# write_bk_file()
-# get_bk_file()
 
-# ET /thshy/index/field/199112/order/desc/page/2/ajax/1/ HTTP/1.1
-# Accept: text/html, */*; q=0.01
-# Accept-Encoding: gzip, deflate
-# Accept-Language: zh-CN,zh;q=0.9
-# Connection: keep-alive
-# Cookie: Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1677976181; log=; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1677990587; v=A8Y0a0jG3tUQg4ox_5QuobmGF7dNJwnF3Go-VLDvtlXFhWgpGLda8az7jleD
-# DNT: 1
-# Host: q.10jqka.com.cn
-# Referer: http://q.10jqka.com.cn/thshy/
-# User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36
-# X-Requested-With: XMLHttpRequest
-# hexin-v: A8Y0a0jG3tUQg4ox_5QuobmGF7dNJwnF3Go-VLDvtlXFhWgpGLda8az7jleD
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Cookie': 'Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1677976181; log=; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1677990587; v=A-YUi6hmuaoeP2rREXrOAVlmN1dtxyqB_Ate5dCP0onkU5iJOFd5kcybrtmj',
+    'Cookie': 'Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1677976181; v=AxPhgB2pczVhxD9hhxaTmhTRopw-yKeKYVzrvsUwbzJpRD0KTZg32nEsew_W',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 }
 current_l = requests.get("http://q.10jqka.com.cn/thshy/index/field/199112/order/desc/page/2/ajax/1/", headers=headers)
